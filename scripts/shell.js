388028a15548cd40974f274662fad9ed7b20d7ea
Jamie <jamiestobbs92@gmail.com>
2013-07-04 13:05:55 +0800

    shell.js

--- scripts/shell.js
+++ scripts/shell.js
@@ -25,11 +25,14 @@ module.exports = function(robot) {
       function(error, stdout, stderr) {
         msg.send(stdout, stderr);
         if (error !== null) {
-          // console.log('exec error: ' + error);
+          console.log('exec error: ' + error);
+        } else {
+          msg.send('Completed with no errors.');
         }
       })
 
   });
+
 
   // ADD USER TO JABBER
   robot.respond(/cmd add (.*) (.*)/i, function(msg) {
@@ -37,26 +40,24 @@ module.exports = function(robot) {
     user = msg.match[1];
     pass = msg.match[2];
 
-    // msg.reply('Username - ' + user);
-    // msg.reply('Password - ' + pass);
-
     child = exec('sudo ejabberdctl register ' + user + ' wiredcraft.teamchat.io ' + pass + '',
       function(error, stdout, stderr) {
         msg.send(stdout, stderr);
         if (error !== null) {
           console.log('exec error: ' + error);
           msg.send('exec error: ' + error);
+        } else {
+          msg.send('Completed with no errors.');
         }
       })
 
   });
 
+
   // REGISTER USER ON JABBER
   robot.respond(/cmd register (.*)/i, function(msg) {
     var user;
     user = msg.match[1];
-
-    // msg.reply('Username - ' + user);
 
     child = exec('sudo ejabberdctl srg_user_add ' + user + ' wiredcraft.teamchat.io Wiredcraft wiredcraft.teamchat.io',
       function(error, stdout, stderr) {
@@ -64,17 +65,18 @@ module.exports = function(robot) {
         if (error !== null) {
           console.log('exec error: ' + error);
           msg.send('exec error: ' + error);
+        } else {
+          msg.send('Completed with no errors.');
         }
       })
 
   });
 
+
   // REMOVE USER FROM JABBER
   robot.respond(/cmd remove (.*)/i, function(msg) {
     var user;
     user = msg.match[1];
-
-    // msg.reply('Username - ' + user);
 
     child = exec('sudo ejabberdctl srg_user_del ' + user +' wiredcraft.teamchat.io Wiredcraft wiredcraft.teamchat.io',
       function(error, stdout, stderr) {
@@ -82,17 +84,18 @@ module.exports = function(robot) {
         if (error !== null) {
           console.log('exec error ' + error);
           msg.send('exec error: ' + error);
+        } else {
+          msg.send('Completed with no errors.');
         }
       })
 
   });
 
+
   // UNREGISTER USER FROM JABBER
   robot.respond(/cmd unregister (.*)/i, function(msg) {
     var user;
     user = msg.match[1];
-
-    // msg.reply('Username - ' + user);
 
     child = exec('sudo ejabberdctl unregister ' + user + ' wiredcraft.teamchat.io',
       function(error, stdout, stderr) {
@@ -100,6 +103,8 @@ module.exports = function(robot) {
         if (error !== null) {
           console.log('exec error ' + error);
           msg.send('exec error: ' + error);
+        } else {
+          msg.send('Completed with no errors.');
         }
       })
 
