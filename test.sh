cc6fdd6c2b54aeb6385974ffd5e62b5cc4fac719
Jamie <jamiestobbs92@gmail.com>
2013-07-09 11:32:20 +0800

    adding config file

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export HUBOT_NAME=Borat
-export HUBOT_ALIAS=/
-
-export HUBOT_GITHUB_TOKEN="c2ea629f0a4619abd039df7e07ecc5bcb7e57917"
-export REDISTOGO_URL="redis://redistogo:a879e80cf4b4ffaba008f4a39892dbf2@beardfish.redistogo.com:9594/"
-
-./bin/hubot
