bfb4e526875e259ae1b75acdb4e9d62c87188790
Jamie <jamiestobbs92@gmail.com>
2013-07-17 16:48:47 +0800

    Remove myself from admins

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ export HUBOT_XMPP_PASSWORD=fghdfiw0*hf
 export HUBOT_XMPP_ROOMS=all@conference.wiredcraft.teamchat.io,dashboard@conference.wiredcraft.teamchat.io
 export HUBOT_NAME=Borat
 export HUBOT_ALIAS=/
-export HUBOT_AUTH_ADMIN=hunvreus,zbal,quentinberder,Makara,Jamesford
+export HUBOT_AUTH_ADMIN=hunvreus,zbal,quentinberder,Makara
 
 export HUBOT_GITHUB_TOKEN="c2ea629f0a4619abd039df7e07ecc5bcb7e57917"
 export REDISTOGO_URL="redis://redistogo:a879e80cf4b4ffaba008f4a39892dbf2@beardfish.redistogo.com:9594/"
