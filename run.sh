93a0bce1450f398baf89aae1f7b29cfb3c233910
Jamie <jamiestobbs92@gmail.com>
2013-07-18 11:09:59 +0800

    adding auth for commands

--- run.sh
+++ run.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-source /etc/profile
-cd /opt/borat
+# source /etc/profile
+# cd /opt/borat
 
 export HUBOT_XMPP_USERNAME=borat@wiredcraft.teamchat.io
 export HUBOT_XMPP_PASSWORD=fghdfiw0*hf
 export HUBOT_XMPP_ROOMS=all@conference.wiredcraft.teamchat.io,dashboard@conference.wiredcraft.teamchat.io
 export HUBOT_NAME=Borat
 export HUBOT_ALIAS=/
-export HUBOT_AUTH_ADMIN=hunvreus,zbal,quentinberder,Makara
+export HUBOT_AUTH_ADMIN=hunvreus,zbal,quentinberder,Makara,1
 
 export HUBOT_GITHUB_TOKEN="c2ea629f0a4619abd039df7e07ecc5bcb7e57917"
 export REDISTOGO_URL="redis://redistogo:a879e80cf4b4ffaba008f4a39892dbf2@beardfish.redistogo.com:9594/"
 
-RUNNING=$(ps aux | grep -i hubot | grep -v grep | wc -l)
-if [ $RUNNING -lt 1 ]; then
-  git pull
-  npm install 
-  ./bin/hubot --adapter xmpp > /dev/null 2>&1 &
-fi
+# RUNNING=$(ps aux | grep -i hubot | grep -v grep | wc -l)
+# if [ $RUNNING -lt 1 ]; then
+#   git pull
+#   npm install 
+#   ./bin/hubot --adapter xmpp > /dev/null 2>&1 &
+# fi
+
+./bin/hubot
--- scripts/test.js
+++ scripts/test.js
@@ -1,15 +1,37 @@
+function isAdmin(user) {    
+    if (process.env.HUBOT_AUTH_ADMIN) {
+        admins = process.env.HUBOT_AUTH_ADMIN.split(',');
+    } else {
+        return false
+    }
+
+    for(i=0; i < admins.length; i++){
+        if(user === admins[i]) {
+            return true
+        }
+    }
+    return false
+}
+
 module.exports = function(robot) {
 
-	robot.respond(/cmd test/i, function(msg) {
-		console.log(robot.brain.data.users);
-		data = robot.brain.data;
-		nicedata = data.toString();
-		msg.send(nicedata);
-	});
+    robot.respond(/storage delete (\w*)$/i, function(msg) {
+        if(isAdmin(msg.message.user.id.toString()) === true) {
+            delete robot.brain.data[msg.match[1]];
+            return msg.send("" + msg.match[1] + " deleted from storage.");
+        } else {
+            return msg.send("Sorry, that is an admin only command");
+        }
+    });
+    
+    // Example of testing for admin
+    robot.respond(/am I admin?/i, function(msg) {
+        admin = isAdmin(msg.message.user.id.toString());
+        if(admin === true) {
+            msg.send('You are indeed an admin.');
+        } else {
+            msg.send('Sorry, you are not an admin.');
+        }
+    });
 
-	robot.respond(/storage delete (\w*)$/i, function(msg) {
-		delete robot.brain.data[msg.match[1]];
-		return msg.send("" + msg.match[1] + " deleted from storage.");
-	});
-	
 };
