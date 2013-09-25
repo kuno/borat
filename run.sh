6e8a73692808e454f3742d17deb2da3335ead3c2
Jamie <jamiestobbs92@gmail.com>
2013-09-25 11:59:47 +0800

    use config files

--- run.sh
+++ run.sh
@@ -3,15 +3,17 @@
 source /etc/profile
 cd /opt/borat
 
-export HUBOT_XMPP_USERNAME=borat@wiredcraft.teamchat.io
-export HUBOT_XMPP_PASSWORD=fghdfiw0*hf
-export HUBOT_XMPP_ROOMS=all@conference.wiredcraft.teamchat.io,dashboard@conference.wiredcraft.teamchat.io
-export HUBOT_NAME=Borat
-export HUBOT_ALIAS=/
-export HUBOT_AUTH_ADMIN=hunvreus,zbal,quentinberder,Makara
+source /config/config.cfg
 
-export HUBOT_GITHUB_TOKEN="c2ea629f0a4619abd039df7e07ecc5bcb7e57917"
-export REDISTOGO_URL="redis://redistogo:a879e80cf4b4ffaba008f4a39892dbf2@beardfish.redistogo.com:9594/"
+export HUBOT_XMPP_USERNAME=$xmppUsername
+export HUBOT_XMPP_PASSWORD=$xmppPassword
+export HUBOT_XMPP_ROOMS=$xmppRooms
+export HUBOT_NAME=$name
+export HUBOT_ALIAS=$alias
+export HUBOT_AUTH_ADMIN=$admins
+
+export HUBOT_GITHUB_TOKEN="$github"
+export REDISTOGO_URL="$redis"
 
 # @wiredcraft Twitter #
 # export WCL_CONSUMER_KEY=""
